8e9565466c841e94ccb4079721966eae3cbf2b6b
mr•vybes <xquame@yahoo.com>
2024-06-27 09:57:38 +0000

    Fix deployment

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,7 +1,7 @@
 // import AppNav from "./AppNav";
 // import { Outlet } from "react-router-dom";
 import Map from "./components/Map";
-import SideBar from "./components/Sidebar";
+import SideBar from "./components/SideBar.jsx";
 import styles from "./pages/AppLayout.module.css";
 import User from "./components/User";
 
